8c238e5bc3a314d3388f70a8cd4464acbda13e24
rissole <bonzaibrother@gmail.com>
2014-06-08 16:55:19 +1000

    added countdown

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var TIMELIMIT = 90 * 1000;
+
 // Make arrow key events send to 2048 frame
 document.addEventListener("keydown", function(event) {
     // if it's an arrow key
@@ -6,3 +8,11 @@ document.addEventListener("keydown", function(event) {
         frame_2048.contentDocument.dispatchEvent(new CustomEvent("keydown", {detail: {event: event}}))
     }
 });
+
+var timerId = countdown((new Date()).getTime()+TIMELIMIT, function(ts) {
+        var seconds = ts.seconds;
+        if (seconds < 10) {
+            seconds = '0'+seconds.toString();
+        }
+        document.getElementById('countdown').innerHTML = ts.minutes + ':'+ seconds;
+    }, countdown.MINUTES|countdown.SECONDS);
