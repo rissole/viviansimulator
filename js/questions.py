80475bc3dda3adb915a759d32f5961c5d9024ba8
rissole <bonzaibrother@gmail.com>
2014-06-08 18:49:44 +1000

    questions in progress

---
+++ js/questions.py
@@ -0,0 +1,76 @@
+questions = [
+    {
+        'video': '-icMszNp38w',
+        'video_desc': 'A promo for Fox\'s smash hit series Bones.',
+        'video_questions': [
+            {
+                'question': 'What season was the episode from this promo in?',
+                'answers': [
+                    ['3', False],
+                    ['4', False],
+                    ['5', False],
+                    ['6', True],
+                ]
+            },
+            {
+                'question': 'What season was the episode from this promo in?',
+                'answers': [
+                    ['3', False],
+                    ['4', False],
+                    ['5', False],
+                    ['6', True],
+                ]
+            },
+            {
+                'question': 'What season was the episode from this promo in?',
+                'answers': [
+                    ['3', False],
+                    ['4', False],
+                    ['5', False],
+                    ['6', True],
+                ]
+            },
+            {
+                'question': 'What season was the episode from this promo in?',
+                'answers': [
+                    ['3', False],
+                    ['4', False],
+                    ['5', False],
+                    ['6', True],
+                ]
+            },
+            {
+                'question': 'What season was the episode from this promo in?',
+                'answers': [
+                    ['3', False],
+                    ['4', False],
+                    ['5', False],
+                    ['6', True],
+                ]
+            },
+        ],
+        'paper': 'http://cs.nyu.edu/~dsontag/papers/AroraEtAl_icml13.pdf',
+        'paper_questions': [
+            {
+                'question': 'Who is the primary author of the paper?',
+                'answer': 'sanjeev arora'
+            },
+            {
+                'question': 'Who is the primary author of the paper?',
+                'answer': 'sanjeev arora'
+            },
+            {
+                'question': 'Who is the primary author of the paper?',
+                'answer': 'sanjeev arora'
+            },
+            {
+                'question': 'Who is the primary author of the paper?',
+                'answer': 'sanjeev arora'
+            },
+            {
+                'question': 'Who is the primary author of the paper?',
+                'answer': 'sanjeev arora'
+            },
+        ]
+    }
+]
