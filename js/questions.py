e4983e2f5b3da197cd7a36d28c8d7c1b581ab777
rissole <bonzaibrother@gmail.com>
2014-06-09 11:42:09 +1000

    second question set framework

--- js/questions.py
+++ js/questions.py
@@ -72,5 +72,79 @@ questions = [
                 'answer': '298'
             },
         ]
+    },
+    {
+        'video': '7hRy2N2CMhQ',
+        'videoDesc': 'A promo for Doctor Who.',
+        'videoQuestions': [
+            {
+                'question': 'What season was the episode from this promo in?',
+                'answers': [
+                    ['3', False],
+                    ['4', False],
+                    ['5', False],
+                    ['6', True],
+                ]
+            },
+            {
+                'question': 'What is the name of the serial killer for the episode?',
+                'answers': [
+                    ['Ghost Assassin', False],
+                    ['Ghost Killer', True],
+                    ['Phantom Renegade', False],
+                    ['Phantom Killer', False],
+                ]
+            },
+            {
+                'question': 'What time did the episode air (American Central Time)?',
+                'answers': [
+                    ['9pm', False],
+                    ['8pm', False],
+                    ['7pm', True],
+                    ['6pm', False],
+                ]
+            },
+            {
+                'question': 'According to the promo, what has the serial killer never done before?',
+                'answers': [
+                    ['Be violent', False],
+                    ['Be untraceable', False],
+                    ['Rip out the victim\'s nails', True],
+                    ['Leave a calling card', False],
+                ]
+            },
+            {
+                'question': 'How many people were sitting around the campfire where the body fell?',
+                'answers': [
+                    ['5', False],
+                    ['4', True],
+                    ['3', False],
+                    ['2', False],
+                ]
+            },
+        ],
+        'paper': 'http://cs.nyu.edu/~dsontag/papers/AroraEtAl_icml13.pdf',
+        'paperQuestions': [
+            {
+                'question': 'Who is the primary author of the paper?',
+                'answer': 'sanjeev arora'
+            },
+            {
+                'question': 'Up to this point, most approaches to topic model learning been based on a(n)...?',
+                'answer': 'maximum likelihood objective'
+            },
+            {
+                'question': 'Posterior inference over document-topic and topic-word distributions is NP-hard. (true/false)',
+                'answer': 'true'
+            },
+            {
+                'question': 'A word that appears in one topic and no others is called a(n)...?',
+                'answer': 'anchor word'
+            },
+            {
+                'question': 'What was the mean document length for the articles from The New York Times?',
+                'answer': '298'
+            },
+        ]
     }
 ]
